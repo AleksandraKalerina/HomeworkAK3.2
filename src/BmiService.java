aaf8ba0d847d7a537cb6528572475e19dd6e9a4a
Aleks Ka <aleksandra.bm@mail.ru>
2024-02-24 23:24:27 +0300

    Third commit

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,8 +1,8 @@
 public class BmiService {
 
     public double calculate(double w, double h) {
-       double calculate;
+        double calculate;
         calculate = w / (h * h);
-       return calculate;
+        return calculate;
     }
 }
