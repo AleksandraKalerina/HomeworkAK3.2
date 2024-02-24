8aaee8ae59dffce9a8ae9a85236bfb2a5fcce528
Aleks Ka <aleksandra.bm@mail.ru>
2024-02-24 15:57:49 +0300

    Second commit

---
+++ src/BmiService.java
@@ -0,0 +1,8 @@
+public class BmiService {
+
+    public double calculate(double w, double h) {
+       double calculate;
+        calculate = w / (h * h);
+       return calculate;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+       //BmiService service = new BmiService();
+       //double bmi = service.calculate(89, 1.98);
+       //System.out.println("Ваш индекс массы тела равен: " + (int)bmi);
+
+        BmiService service = new BmiService();
+
+        System.out.println();
+        System.out.println("Ваш индекс массы тела равен: " + (int)service.calculate(98, 1.98));
+
+        System.out.println();
+        System.out.println("Ваш индекс массы тела равен: " + (int)service.calculate(76, 1.58));
+
+        System.out.println();
+        System.out.println("Ваш индекс массы тела равен: " + (int)service.calculate(58, 1.88));
+
+
+    }
+}
